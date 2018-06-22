f5633dc1711d4b7e4a72aec0b7feed571e9045aa
Mike.McLean <Mike.McLean@softwire.com>
2018-06-22 16:15:50 +0100

    Parent Component State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ParentComponent from './ParentComponent';
 
 class App extends Component {
 
@@ -14,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ParentComponent/>
       </div>
     );
   }
