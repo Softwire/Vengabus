ea321e8ba5bfcf73e1116f678de60952f78cded1
Alice Wenban <Alice.Wenban@softwire.com>
2018-07-13 12:02:58 +0100

    made create folder first commit

---
+++ src/Nav/SideBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
+import { PAGES, pageSwitcher } from './PageSwitcherService';
+import { HomePage } from './HomePage';
+import { OtherPage } from './OtherPage';
+
+export class VengaBusSideBar extends Component {
+
+    render() {
+        return (
+           <div></div>
+        );
+    }
+}
