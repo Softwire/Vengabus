e5fb55411c56bf80784a628055dddf371cf8a9c3
Mike.McLean <Mike.McLean@softwire.com>
2018-07-13 16:17:08 +0100

    Add clarifying comments to the ServiceBusWrapper

--- src/AzureWrappers/VengaServiceBusService.js
+++ src/AzureWrappers/VengaServiceBusService.js
@@ -5,13 +5,14 @@ require('util.promisify').shim();
 /*
     This class exists as our own wrapper around the equivalent Azure API object,
     so that we can make any appropriate improvements to it's API.
-    e.g.
-    Converting everything to promises.
+    e.g. Converting everything to promises.  
 */
 export class VengaServiceBusService {
     constructor(connectionString) {
         this.rawService = azure.createServiceBusService(connectionString);
     }
 
+    /* Note that the lamda here captures `this` = the VengaServiceBusService, to access rawService.
+       But it also captures `rawService` as the callee of getQueue (and hence, the value of `this` INSIDE the getQueue method)*/
     getQueue = util.promisify((queueName, callback) => this.rawService.getQueue(queueName, callback));
 }
