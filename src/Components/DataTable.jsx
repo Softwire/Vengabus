1698b0fbff78bd15bd20c7aa24e80de352667fb2
Mate Kovacs <Mate.Kovacs@softwire.com>
2018-07-17 11:22:40 +0100

    Added optional prop for row events

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -1,33 +1,47 @@
-import React, { Component } from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
-import { css } from 'react-emotion';
+import React, { Component } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import { css } from "react-emotion";
 /*
 Returns a table created from an input object
-Required props:
-    DataToDisplay: the object which contains the data to be displayed in the table
+Props:
+    DataToDisplay: the object which contains the data to be displayed in the table (REQUIRED)
     ColProps: contains properties regarding specific columns (format: array of objects)
-        dataField: specifies the name of the property of DataToDisplay which is to be displayed in the column
-        text: text which is to be displayed in the header of the column
-        headerStyle: object which contains CSS styling for the header
+        dataField: specifies the name of the property of DataToDisplay which is to be displayed in the column (REQUIRED)
+        text: text which is to be displayed in the header of the column (REQUIRED)
+        headerStyle: object which contains CSS styling for the header (RECOMMENDED)
+    RowEvents: Object which contains functions that are called on certain events of the row (OPTIONAL). For example:
+                RowEvents = {
+                    onClick: (row, rowIndex, e) => {
+                        ....
+                    }
+                }
+                where row is an object which contains all the information which is in that row
 
 For more info see:
-https://react-bootstrap-table.github.io/react-bootstrap-table2/docs/basic-column.html
+https://react-bootstrap-table.github.io/react-bootstrap-table2/
 */
 export class DataTable extends Component {
-    render() {
-        const ColProps = this.props.ColProps;
-        const DataToDisplay = this.props.DataToDisplay;
+  render() {
+    const ColProps = this.props.ColProps;
+    const DataToDisplay = this.props.DataToDisplay;
+    const RowEvents = this.props.RowEvents;
 
-        const tableRowStyle = css`
-            :hover {
-                background-color: yellow;
-            }
-        `;
+    const tableRowStyle = css`
+      :hover {
+        background-color: yellow;
+      }
+    `;
 
-        return DataToDisplay ? 
-            <BootstrapTable keyField="number" data={DataToDisplay} rowClasses={tableRowStyle} columns={ColProps} />
-         : 
-            <p>No data has been retrieved yet.</p>
-        ;
-    }
+    return DataToDisplay ? (
+      <BootstrapTable
+        keyField="number"
+        data={DataToDisplay}
+        rowClasses={tableRowStyle}
+        columns={ColProps}
+        rowEvents={RowEvents}
+      />
+    ) : (
+      <p>No data has been retrieved yet.</p>
+    );
+  }
 }
--- src/Components/QueueList.jsx
+++ src/Components/QueueList.jsx
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
-import { DataTable } from './DataTable';
+import React, { Component } from "react";
+import { DataTable } from "./DataTable";
 
 export class QueueList extends Component {
-    onRowClick(row) {
-        window.open('http://google.com', '_blank');
-    }
+  onRowClick(row) {
+    window.open("http://google.com", "_blank");
+  }
 
-    render() {
-        const queueArray = this.props.queueData;
+  render() {
+    const queueArray = this.props.queueData;
 
-        const ColProps = [
-            {
-                dataField: 'number',
-                text: 'Number',
-                headerStyle: { width: '10%' }
-            },
-            {
-                dataField: 'name',
-                text: 'Name',
-                headerStyle: { width: '50%' }
-            },
-            {
-                dataField: 'status',
-                text: 'Status',
-                headerStyle: { width: '40%' }
-            }
-        ];
+    const ColProps = [
+      {
+        dataField: "number",
+        text: "Number",
+        headerStyle: { width: "10%" }
+      },
+      {
+        dataField: "name",
+        text: "Name",
+        headerStyle: { width: "50%" }
+      },
+      {
+        dataField: "status",
+        text: "Status",
+        headerStyle: { width: "40%" }
+      }
+    ];
 
-        return <DataTable ColProps={ColProps} DataToDisplay={queueArray} onRowClick={this.onRowClick} />;
-    }
+    const RowEvents = {
+      onClick: (e, row, rowIndex) => {
+        console.log(row);
+      }
+    };
+
+    return (
+      <DataTable
+        ColProps={ColProps}
+        DataToDisplay={queueArray}
+        RowEvents={RowEvents}
+      />
+    );
+  }
 }
