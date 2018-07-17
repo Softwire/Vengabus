82c13e0b8c8300bf23de4e20977fa26686aac576
Jamie Fox <jamie.fox@softwire.com>
2018-07-17 14:40:45 +0100

    onClick function now passed as a prop, separate from RowEvents, to DataTable. Also centred header text in the table

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
-import { css } from "react-emotion";
 /*
 Returns a table created from an input object
 Props:
@@ -11,32 +10,40 @@ Props:
         headerStyle: object which contains CSS styling for the header (RECOMMENDED)
     RowEvents: Object which contains functions that are called on certain events of the row (OPTIONAL). For example:
                 RowEvents = {
-                    onClick: (row, rowIndex, e) => {
-                        ....
-                    }
+                    
                 }
                 where row is an object which contains all the information which is in that row
+    Note: please define onClick events in the function "rowClickResponseFunction" and pass as a prop to DataTable.
 
 For more info see:
 https://react-bootstrap-table.github.io/react-bootstrap-table2/
 */
 export class DataTable extends Component {
-  render() {
-    const ColProps = this.props.ColProps;
-    const DataToDisplay = this.props.DataToDisplay;
-    const RowEvents = this.props.RowEvents;
-    const tableRowStyle = this.props.tableRowStyle;
+    render() {
+        const ColProps = this.props.ColProps;
+        const DataToDisplay = this.props.DataToDisplay;
+        const tableRowStyle = this.props.tableRowStyle;
+        const RowEvents = this.props.RowEvents;
+        const rowClickResponseFunction = this.props.OnClick;
 
-    return DataToDisplay ? (
-      <BootstrapTable
-        keyField={ColProps[0].dataField} //use the left-most column as the keyfield for the table
-        data={DataToDisplay}
-        rowClasses={tableRowStyle}
-        columns={ColProps}
-        rowEvents={RowEvents}
-      />
-    ) : (
-        <p>No data has been retrieved yet.</p>
-      );
-  }
+        if (RowEvents.onClick && rowClickResponseFunction) { //if 2 onClick functions are defined
+            throw new Error("Error: the row response to being clicked on is defined multiple times");
+        }
+        const onClickFunction = rowClickResponseFunction ? rowClickResponseFunction : RowEvents.onClick;
+        //if there is an onClick response, add this to RowEvents
+        const finalRowEvents = onClickFunction ? { ...(this.props.RowEvents), onClick: onClickFunction } : RowEvents;
+
+
+        return DataToDisplay ? (
+            <BootstrapTable
+                keyField={ColProps[0].dataField} //use the left-most column as the keyfield for the table
+                data={DataToDisplay}
+                rowClasses={tableRowStyle}
+                columns={ColProps}
+                rowEvents={finalRowEvents}
+            />
+        ) : (
+                <p>No data has been retrieved yet.</p>
+            );
+    }
 }
--- src/Components/QueueList.jsx
+++ src/Components/QueueList.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { DataTable } from "./DataTable";
 import { css } from 'react-emotion';
-import { paleGreyBlue, mediumGreyBlue, palerBlue } from '../colourScheme';
+import { paleGreyBlue, palerBlue } from '../colourScheme';
 
 export class QueueList extends Component {
+    rowClickResponseFunction(e, row, rowIndex) { //pass this function as a prop to DataTable, call it when "row" is clicked on
+        console.log(row);
+    }
+
     render() {
         const queueArray = this.props.queueData;
 
@@ -11,32 +15,33 @@ export class QueueList extends Component {
             {
                 dataField: "number",
                 text: "Number",
-                headerStyle: { width: "10%" }
+                headerStyle: {
+                    width: "10%", textAlign: "center"
+                }
             },
             {
                 dataField: "name",
                 text: "Name",
-                headerStyle: { width: "50%" }
+                headerStyle: { width: "50%", textAlign: "center" }
             },
             {
                 dataField: "status",
                 text: "Status",
-                headerStyle: { width: "40%" }
+                headerStyle: { width: "40%", textAlign: "center" }
             }
         ];
 
         const RowEvents = {
-            onClick: (e, row, rowIndex) => {
-                console.log(row);
-            }
-        };
+        }
 
         const tableRowStyle = css`
 		          :hover {
 		              border: 2px solid ${palerBlue};
 		              background-color: ${paleGreyBlue};
 		          }
-		      `;
+              `;
+
+
 
         return (
             <DataTable
@@ -44,6 +49,7 @@ export class QueueList extends Component {
                 DataToDisplay={queueArray}
                 RowEvents={RowEvents}
                 tableRowStyle={tableRowStyle}
+                OnClick={this.rowClickResponseFunction}
             />
         );
     }
