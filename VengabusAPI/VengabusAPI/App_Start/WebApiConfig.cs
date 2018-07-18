03ee3b16b39e03f01aa5775c74b84680e0b98c1b
Mike.McLean <Mike.McLean@softwire.com>
2018-07-18 16:55:47 +0100

    Initial API setup

---
+++ VengabusAPI/VengabusAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace VengabusAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.EnableCors(new EnableCorsAttribute(origins: "*", headers: "*", methods: "*"));
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            json.SerializerSettings.Converters = new JsonConverter[] { new StringEnumConverter() };
+            json.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+
+        }
+    }
+}
