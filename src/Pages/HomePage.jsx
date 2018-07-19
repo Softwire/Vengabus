2266f4280431d950373aa21eda83555ff8af6779
Mate Kovacs <Mate.Kovacs@softwire.com>
2018-07-19 14:13:46 +0100

    Removed ExampleServiceBusDataDisplay and moved QueueList to the home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ExampleServiceBusCall } from '../Components/ExampleServiceBusCall';
-import { ExampleServiceBusDataDisplay } from '../Components/ExampleServiceBusDataDisplay';
+import { QueueList } from '../Components/QueueList';
+import { css } from 'react-emotion';
 
 export class HomePage extends Component {
     constructor(props) {
@@ -17,6 +18,11 @@ export class HomePage extends Component {
     };
 
     render() {
+        const queueDivStyle = css`
+            width: 30%;
+            margin: 10px;
+            display: inline-block; /*to allow tables to be displayed side by side*/
+        `;
         return (
             <div>
                 <ExampleServiceBusCall onDataReceive={this.updateRetrievedData} />
@@ -25,7 +31,11 @@ export class HomePage extends Component {
                     // Once the API is working we need to figure out how to best pass in data about the list of queues and topics
                     // For now it is assumed that everything is passed in in one object
                 }
-                <ExampleServiceBusDataDisplay data={this.state.retrievedData} />
+                <div className={queueDivStyle}>
+                    <p> I got back: '{JSON.stringify(this.state.retrievedData)}' </p>
+                    <QueueList queueData={this.state.retrievedData} />
+                </div>
+                );
             </div>
         );
     }
