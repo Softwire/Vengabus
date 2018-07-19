ab66923c1e2935de7ddd3bc129aabf884f0c4d52
AliceWenban <wenbanalice@gmail.com>
2018-07-19 11:31:14 +0100

    made Format box only indetation not colour

---
+++ VengabusFrontend/src/Components/PrettyXmlBox.jsx
@@ -0,0 +1,28 @@
+// a component that just takes in some xml and returns it in a div
+
+import React, { Component } from 'react';
+import { css } from 'react-emotion';
+const format = require("xml-formatter");
+
+export class PrettyXMLBox extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        const formattedXml = format(this.props.xml);
+
+        const formatCss = css`
+            text-align: left;
+        `;
+
+        return (
+            <div >
+                <pre className={formatCss}>
+                    {formattedXml}
+                </pre>
+            </div>
+        );
+    }
+}
