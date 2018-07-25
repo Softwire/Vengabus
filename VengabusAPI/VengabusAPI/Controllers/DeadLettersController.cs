a425e98d9263fa14f75708ebafcf8625f6805145
Jamie Fox <jamie.fox@softwire.com>
2018-07-25 10:12:00 +0100

    started work on ViewSubscriptionDeadLetterMessage (NOT working yet)

--- VengabusAPI/VengabusAPI/Controllers/DeadLettersController.cs
+++ VengabusAPI/VengabusAPI/Controllers/DeadLettersController.cs
@@ -37,8 +37,6 @@ namespace VengabusAPI.Controllers
             var namespaceManager = new NamespaceManager(address, GetSASToken());
             var deadLetterMessageCount = namespaceManager.GetQueue(queueName).MessageCountDetails.DeadLetterMessageCount;
 
-            Console.WriteLine(deadLetterMessageCount);
-
             IEnumerable<VengaMessage> messageOutputIEnum = Enumerable.Empty<VengaMessage>();
 
             /*
@@ -54,21 +52,20 @@ namespace VengabusAPI.Controllers
 
             while (i < deadLetterMessageCount)
             {
-                IEnumerable<BrokeredMessage> Messages;
+                IEnumerable<BrokeredMessage> messages;
                 if (initialPeekBatch)
                 {
                     initialPeekBatch = false;
-                    Messages = queueClient.PeekBatch((int)deadLetterMessageCount).ToList();
+                    messages = queueClient.PeekBatch((int)deadLetterMessageCount).ToList();
                 }
                 else
                 {
-                    Messages = queueClient.PeekBatch(lastSequenceNumber, (int)deadLetterMessageCount - i).ToList();
+                    messages = queueClient.PeekBatch(lastSequenceNumber, (int)deadLetterMessageCount - i).ToList();
                 }
 
-                foreach (var message in Messages)
+                foreach (var message in messages)
                 {
                     i++;
-                    Console.WriteLine("1 message");
                     var vengaMessage = new VengaMessage(message.Properties, message.GetBody<String>(),
                         message.MessageId, message.ContentType);
                     messageOutputIEnum = messageOutputIEnum.Concat(new[] {vengaMessage});
@@ -84,7 +81,57 @@ namespace VengabusAPI.Controllers
         [Route("deadLetters/subscription/{subscriptionName}")]
         public void ViewSubscriptionDeadLetterMessages(string subscriptionName)
         {
-            throw new NotImplementedException();
+            /*int i = 0;
+            var clientFactory = CreateEndpointSenderFactory();
+
+            SubscriptionClient subscriptionClientMain = clientFactory.CreateSubscriptionClient(subscriptionName);
+            var deadLetterSubscriptionPath = SubscriptionClient.FormatDeadLetterPath(subscriptionClientMain.Path);
+
+            var subscriptionClient = clientFactory.CreateSubscriptionClient(deadLetterSubscriptionPath);
+            const string address = "https://vengabusdemo.servicebus.windows.net/";
+            var namespaceManager = new NamespaceManager(address, GetSASToken());
+            var deadLetterMessageCount = namespaceManager.GetQueue(subscriptionName).MessageCountDetails.DeadLetterMessageCount;
+            */
+            IEnumerable<VengaMessage> messageOutputIEnum = Enumerable.Empty<VengaMessage>();
+
+
+            /*
+             * The problem here is that, QueueClient.Peekbatch(number) does not guarantee to return the specified number
+             * of messages. Instead, the given number is just an upper bound. We have to record the Sequence Number
+             * of the last received message from the previous call of Peekbatch(), and call it again starting from 
+             * the previous Sequence Message + 1 in order to read new messages, until we peeked at the correct number 
+             * of messages.
+             * */
+
+            /*
+            long lastSequenceNumber = 0;
+
+            var initialPeekBatch = true;
+
+            while (i < deadLetterMessageCount)
+            {
+                IEnumerable<BrokeredMessage> messages;
+                if (initialPeekBatch)
+                {
+                    initialPeekBatch = false;
+                    messages = queueClient.PeekBatch((int)deadLetterMessageCount).ToList();
+                }
+                else
+                {
+                    messages = queueClient.PeekBatch(lastSequenceNumber, (int)deadLetterMessageCount - i).ToList();
+                }
+
+                foreach (var message in messages)
+                {
+                    i++;
+                    var vengaMessage = new VengaMessage(message.Properties, message.GetBody<String>(),
+                        message.MessageId, message.ContentType);
+                    messageOutputIEnum = messageOutputIEnum.Concat(new[] { vengaMessage });
+                    lastSequenceNumber = message.SequenceNumber + 1;
+                }
+            }
+            */
+            return messageOutputIEnum;
         }
 
     }
