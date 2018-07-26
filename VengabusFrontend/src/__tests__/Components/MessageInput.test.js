0e657d7a33d9547b8b0306d610ff8ca801875b2a
Mate Kovacs <Mate.Kovacs@softwire.com>
2018-07-26 14:38:04 +0100

    Added tests

--- VengabusFrontend/src/__tests__/Components/MessageInput.test.js
+++ VengabusFrontend/src/__tests__/Components/MessageInput.test.js
@@ -3,7 +3,7 @@ import Adaptor from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
 import React from 'react';
-import { Button } from "react-bootstrap";
+import { FormControl, Button } from "react-bootstrap";
 import { MessageInput } from '../../Components/MessageInput';
 import { MessagePropertyInputRow } from '../../Components/MessagePropertyInputRow';
 
@@ -34,8 +34,37 @@ it('Deletes a property when the delete button is pressed', () => {
     expect(wrapper.find(MessagePropertyInputRow).length).toEqual(2);
 });
 
+it('Discard Message button works', () => {
+    let wrapper = mount(<MessageInput />);
+    wrapper.setState({
+        userDefinedProperties: [{ name: "test1", value: "any value 1" }, { name: "test2", value: "any value 2" }, { name: "test3", value: "any value 3" }],
+        preDefinedProperties: [{ name: 'MessageID', value: 'id' }],
+        messageBody: 'Hello World!'
+    });
+    let discardButton = wrapper.find(Button).last();
+    discardButton.simulate('click');
+    expect(wrapper.find(MessagePropertyInputRow).length).toEqual(0);
+    let body = wrapper.find(FormControl);
+    expect(body.value).toBeUndefined();
+});
+
 it('renders correctly', () => {
     let messagePropertyInput = renderer.create(
         <MessageInput />);
+    expect(messagePropertyInput.toJSON()).toMatchSnapshot();
+});
+
+it('renders correctly from a predefined message', () => {
+    const message = {
+        MessageProperties: {
+            userDefinedProp1: 'value1',
+            userDefinedProp2: 'value2'
+        },
+        MessageBody: 'Hello world!',
+        MessageId: 'Message1',
+        ContentType: 'null'
+    };
+    let messagePropertyInput = renderer.create(
+        <MessageInput message={message}/>);
     expect(messagePropertyInput.toJSON()).toMatchSnapshot();
 });
