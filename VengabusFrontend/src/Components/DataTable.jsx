e1f91d0463ed69ff14d6f2bb068f0eb8ef1c0687
Mate Kovacs <Mate.Kovacs@softwire.com>
2018-07-26 16:29:40 +0100

    Added demo showing how jsx elements can be added into a table

--- VengabusFrontend/src/Components/DataTable.jsx
+++ VengabusFrontend/src/Components/DataTable.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Button, Component } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import { palerBlue } from '../colourScheme';
 
@@ -25,25 +25,14 @@ https://react-bootstrap-table.github.io/react-bootstrap-table2/
 */
 export class DataTable extends Component {
 
-
     render() {
         const colProps = this.props.colProps;
         const dataToDisplay = this.props.dataToDisplay;
         const tableRowStyle = this.props.tableRowStyle;
         const rowEvents = this.props.rowEvents || {};
         const name = this.props.name;
-        const select = this.props.rowSelect;
         let onRowClick = this.props.onRowClick;
         let finalRowEvents = undefined;
-
-        let selectRowProp = {
-            mode: 'checkbox',
-            bgColor: palerBlue, // you should give a bgcolor, otherwise, you can't regonize which row has been selected
-            hideSelectColumn: true,  // enable hide selection column.
-            clickToSelect: false,  // you should enable clickToSelect, otherwise, you can't select column.
-            selected: [select]
-        };
-
 
         if (dataToDisplay) {
             if (!colProps) {
@@ -57,7 +46,6 @@ export class DataTable extends Component {
             finalRowEvents = { ...(rowEvents), onClick: onRowClick };
         }
 
-
         return dataToDisplay ? (
             <BootstrapTable
                 keyField={colProps[0].dataField} //use the left-most column as the keyfield for the table
@@ -65,7 +53,6 @@ export class DataTable extends Component {
                 rowClasses={tableRowStyle}
                 columns={colProps}
                 rowEvents={finalRowEvents}
-                selectRow={selectRowProp}
             />
         ) : (
                 <p>No data has been retrieved yet.</p>
