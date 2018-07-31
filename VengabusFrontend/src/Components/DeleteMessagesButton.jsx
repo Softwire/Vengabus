987357efdba4cbc6e3181d40062c284c4e4f18be
Ion Birsu <ion.birsu@softwire.com>
2018-07-31 10:33:50 +0100

    Add popup alert for deleting all messages

--- VengabusFrontend/src/Components/DeleteMessagesButton.jsx
+++ VengabusFrontend/src/Components/DeleteMessagesButton.jsx
@@ -1,36 +1,76 @@
-import Dialog from "react-bootstrap-dialog";
 import React from "react";
-import { Glyphicon, Button } from "react-bootstrap";
+import { Glyphicon, Modal, Alert, Button } from "react-bootstrap";
+import { serviceBusConnection } from '../AzureWrappers/ServiceBusConnection';
+import { EndpointTypes } from '../Helpers/EndpointType';
 
-export default class DeleteMessageButton extends React.Component {
+class DeleteMessagesButton extends React.Component {
   constructor(props) {
     super(props);
-    this.alertPopup = this.alertPopup.bind(this);
+
+    this.state = {
+      show: false,
+      numberOfMessages: 0
+    };
   }
 
-  alertPopup() {
-    this.dialog.show({
-      body: "Are you sure you want to delete all the messages ?",
-      actions: [
-        Dialog.DefaultAction("Delete", this.props.onClick, "btn-danger"),
-        Dialog.CancelAction()
-      ]
+  handleClose = () => {
+    this.setState({
+      show: false,
+      numberOfMessages: 0
+    });
+  }
+
+  handleOpening = () => {
+    let vengaServiceBusService = serviceBusConnection.getServiceBusService();
+    let promiseResponse;
+    switch (this.props.type) {
+      case EndpointTypes.QUEUE:
+        promiseResponse = vengaServiceBusService.getQueueDetails(this.props.endpointName);
+        break;
+      case EndpointTypes.TOPIC:
+        promiseResponse = vengaServiceBusService.getTopicDetails(this.props.endpointName);
+        break;
+      case EndpointTypes.SUBSCRIPTION:
+        promiseResponse = vengaServiceBusService.getSubscriptionDetails(this.props.parentName, this.props.endpointName);
+        break;
+      default: break;
+    }
+
+    promiseResponse.then((response) => {
+      this.setState({
+        show: true,
+        numberOfMessages: response.data.activeMessageCount
+      });
     });
   }
 
   render() {
     return (
       <div>
-        <Button onClick={this.alertPopup} bsStyle="danger">
-          Delete messages&nbsp;
-          <Glyphicon glyph="trash" />
+        <Button onClick={this.handleOpening} bsStyle="danger">
+          Delete ALL messages&nbsp;
+            <Glyphicon glyph="trash" />
         </Button>
-        <Dialog
-          ref={el => {
-            this.dialog = el;
-          }}
-        />
+        <Modal show={this.props && this.state.show} onHide={this.handleClose} >
+          <Modal.Header>
+            <Modal.Title>Delete all messages from {this.props.type}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <Alert bsStyle="danger">
+              <p>Are you sure you want to delete all the messages from the {this.props.endpointName} ?</p>
+              <p>{this.state.numberOfMessages} messages will be deleted <b>irreversibly</b>!</p >
+            </Alert >
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.props.onClick} bsStyle="danger" >Delete </Button>
+            <Button onClick={this.handleClose} >Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
 }
+
+export { DeleteMessagesButton };
