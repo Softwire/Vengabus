b6726b0d7fcb741f98dcb7eeb66064a43d27b13d
Mike.McLean <Mike.McLean@softwire.com>
2018-07-31 19:04:46 +0100

    Clarify the intended usage of testHelpers.afterReactHasUpdated()

--- VengabusFrontend/src/Helpers/testHelper.js
+++ VengabusFrontend/src/Helpers/testHelper.js
@@ -1,8 +1,19 @@
 class TestHelper {
     /*
-     * It waits until the state of the components is updated and then calls the callback function.
+     * It returns a promise that waits until the state of the components is updated and then continues.
+     * 
+     * Expected usage is: 
+     *     return testHelper.afterReactHasUpdated().then(() => {
+     *         expect('actualValue').toEqual('expectedValue');
+     *     });
+     * 
+     * Notes on multi-layered async tests (e.g. wait for this, and then wait for that, and then expect a result) will be forthcoming.
      */
-    afterReactHasUpdated(callback) {
+    afterReactHasUpdated(noArgsGiven) {
+        if (noArgsGiven) {
+            throw new Error('afterReactHasUpdated() does not expect to take any arguments. See expected usage for further details.');
+        }
+
         return new Promise(function (resolve) {
             setTimeout(resolve, 0);
         });
