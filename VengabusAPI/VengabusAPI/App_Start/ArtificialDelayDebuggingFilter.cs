b16ae652f9b41933d518e76320d03f0d50cd43e5
Mike.McLean <Mike.McLean@softwire.com>
2018-08-01 15:42:02 +0100

    Add clarifying comments for ArtificialDelayDebuggingFilter

--- VengabusAPI/VengabusAPI/App_Start/ArtificialDelayDebuggingFilter.cs
+++ VengabusAPI/VengabusAPI/App_Start/ArtificialDelayDebuggingFilter.cs
@@ -3,12 +3,17 @@ using System.Web.Http.Filters;
 
 namespace VengabusAPI.Startup
 {
+    /// <summary>
+    /// This is a filter to be used whilst debugging async behaviour on the FrontEnd.
+    /// It inserts an artificial delay on all endpoint calls.
+    /// </summary>
     public class ArtificialDelayDebuggingFilter : ActionFilterAttribute
     {
         public static bool IsActive = false;
         private int globalSecondsDelay = 5;
         public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
         {
+            //Pause for some period, and then continue as usual.
             Task.Delay(globalSecondsDelay * 1000).Wait();
             base.OnActionExecuting(actionContext);
         }
