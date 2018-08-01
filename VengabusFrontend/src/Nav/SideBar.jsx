50dbedfda782fac17922b90e513c734a9e435f4e
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-08-01 11:28:14 +0100

    Removed hard-coded height on sidebar

--- VengabusFrontend/src/Nav/SideBar.jsx
+++ VengabusFrontend/src/Nav/SideBar.jsx
@@ -9,9 +9,10 @@ import { grey } from '../colourScheme';
 
 export class SideBar extends Component {
     render() {
+        //Navbar is 50px height, sidebar height must fit below that, therefore 100% of viewport height - 50px
         const backDiv = css`
             width: 15%;
-            height: 1080px;
+            min-height: calc(100vh - 50px);
             color: white;
             background: ${grey};
             float: left;
