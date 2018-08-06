e7601d26d34ec796c6eeed835ebe05de4a21a1f2
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-08-06 17:46:07 +0100

    Improved animation

--- VengabusFrontend/src/Components/Konamibus.jsx
+++ VengabusFrontend/src/Components/Konamibus.jsx
@@ -12,7 +12,7 @@ export class Konamibus extends Component {
             vengaVideo: false
         };
 
-        this.animationDurationSeconds = 10;
+        this.animationDurationSeconds = 12;
         new KonamiCode(() => {
             this.setState({
                 vengabus: true
@@ -25,16 +25,16 @@ export class Konamibus extends Component {
         });
     }
     render() {
-        const containerStyle = css`
-            perspective: 1000px;
-        `;
         const fixedPosition = css`
             position: fixed;
+            perspective: 1000px;
         `;
 
         const offScreen = 'translateX(calc(-50% - 250px))';
-        const onScreen = 'translateX(calc(-50% + 50px))';
+        const onScreen = 'translateX(calc(-50% + 75px))';
+        const halfOnScreen = 'translateX(calc(-50% - 125px))';
         const facingRight = 'rotateY(0deg)';
+        const facingAway = 'rotateY(150deg)';
         const facingLeft = 'rotateY(180deg)';
 
         //Lowers the sidebar so the bus can peek past.
@@ -42,7 +42,7 @@ export class Konamibus extends Component {
         //that it doesn't scroll with the page, but this means that changing it's height will not move other
         //components out of the way.
         const heightChanger = css`
-            --final-height: 180px;
+            --final-height: 180px; /*css variable */
             @keyframes Height-Change {
                 0% {
                     height: 0px;
@@ -59,34 +59,82 @@ export class Konamibus extends Component {
         transform-style: preserve-3d;
         animation: Height-Change ${this.animationDurationSeconds}s linear;
         `;
-        const busPeek = css`
-            @keyframes Bus-Peek {
+        const busPositionX = css`
+            @keyframes Bus-Position-X {
                 0%, 20% {
-                    transform: ${offScreen} ${facingRight};
+                    transform: ${offScreen};
+                    animation-timing-function: cubic-bezier(0.5, -0.3, 0.5, 1.3);
                 }
-                40%, 60% {
-                    transform: ${onScreen} ${facingRight};
+                40%, 55% {
+                    transform: ${halfOnScreen};
+                    animation-timing-function: cubic-bezier(.45,.18,.52,.51);
+                }
+                60% {
+                    transform: ${onScreen};
+                    animation-timing-function: ease-out
                 }
                 70% {
-                    transform: ${onScreen} ${facingLeft};
+                    transform: translateX(calc(-50% + 450px));
+                    animation-timing-function: cubic-bezier(0.470, 0.000, 0.745, 0.715); /* easeInSine */
                 }
                 80% {
-                    transform: ${offScreen} ${facingLeft};
+                    transform: ${offScreen};
+                    visibility: hidden; /* Prevents the bus from flashing when the animation ends */
                 }
                 100% {
-                    transform: ${offScreen} ${facingLeft};
+                    transform: ${offScreen};
                     visibility: hidden; /* Prevents the bus from flashing when the animation ends */
                 }
             }
             transform-style: preserve-3d;
-            animation: Bus-Peek ${this.animationDurationSeconds}s linear;
+            animation: Bus-Position-X ${this.animationDurationSeconds}s linear;
+        `;
+
+        const busPositionZ = css`
+            @keyframes Bus-Position-Z {
+                0%, 60% {
+                    transform: translateZ(0px);
+                    animation-timing-function: cubic-bezier(0.470, 0.000, 0.745, 0.715); /* easeInSine */
+                }
+                70% {
+                    transform: translateZ(-100px);
+                    animation-timing-function: cubic-bezier(0.390, 0.575, 0.565, 1.000); /* easeOutSine */
+                }
+                80%, 100% {
+                    transform:  translateZ(-150px);
+                }
+            }
+            transform-style: preserve-3d;
+            animation: Bus-Position-Z ${this.animationDurationSeconds}s linear;
+        `;
+        const busRotation = css`
+            @keyframes Bus-Rotation {
+                0%, 55% {
+                    transform: ${facingRight};
+                }
+                60% {
+                    transform: ${facingRight};
+                }
+                70% {
+                    transform: ${facingAway};
+                }
+                80%, 100% {
+                    transform: ${facingLeft};
+                }
+            }
+            transform-style: preserve-3d;
+            animation: Bus-Rotation ${this.animationDurationSeconds}s linear;
         `;
         if (this.state.vengabus) {
             return (
-                <span className={containerStyle}>
+                <span>
                     <div className={fixedPosition}>
-                        <div className={busPeek}>
-                            <img src={bus} className="Vengabus" alt="Vengabus" />
+                        <div className={busPositionX}>
+                            <div className={busPositionZ}>
+                                <div className={busRotation}>
+                                    <img src={bus} className="Vengabus" alt="Vengabus" />
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className={heightChanger} />
