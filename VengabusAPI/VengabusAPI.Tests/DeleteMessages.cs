01a508bc63fbd9ec9889ab8459c0af41a3d02549
raoxiaojia <raoxiaojia@gmail.com>
2018-08-06 14:38:06 +0100

    Added unit tests in backend for sending and deleting messages in queue

---
+++ VengabusAPI/VengabusAPI.Tests/DeleteMessages.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VengabusAPI.Controllers;
+using VengabusAPI.Models;
+using VengabusAPI.Services;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace VengabusAPI.Tests
+{
+    [TestClass]
+    public class DeleteMessages
+    {
+        [TestMethod, Description("The correct number of messages should be sent to a queue")]
+        public void SendMessagesToQueue()
+        {
+            var initialMessageCount = TestHelper.getMessageCountInQueue();
+
+            //arrange
+            //prepare for the request
+            var request = new HttpRequestMessage();
+
+            var predefinedProperties = new Dictionary<string, object>();
+            predefinedProperties.Add("MessageId", "asdsdsasdasdsda");
+
+            var customProperties = new Dictionary<string, object>();
+            customProperties.Add("MessaasdasdgeId", "asdsdsaasddsdasdsda");
+
+            var controller = new MessagesController();
+            controller.Request = request;
+            controller.Request.Headers.Add("Auth-SAS", TestHelper.sasString);
+
+            var msg = new VengaMessage(customProperties,predefinedProperties,"adkajslkjdslakd");
+
+            var messageCount = 10;
+
+            //act
+            for (var i = 0; i < messageCount; i++)
+            {
+                controller.SendMessageToQueue(TestHelper.TestQueueName, msg);
+            }
+
+            //assert
+            var finalMessageCount = TestHelper.getMessageCountInQueue();
+            Assert.AreEqual(finalMessageCount.ActiveMessageCount - initialMessageCount.ActiveMessageCount, messageCount);
+        }
+
+        [TestMethod, Description("All messages should be deleted from queue")]
+        public void DeleteAllMessagesFromQueue()
+        {
+            //arrange
+            //prepare the messages to be deleted
+            TestHelper.SendMessagesToQueue(10);
+
+            //prepare for the request
+            var request = new HttpRequestMessage();
+
+            var controller = new MessagesController();
+            controller.Request = request;
+            controller.Request.Headers.Add("Auth-SAS", TestHelper.sasString);
+
+            //act
+            controller.DeleteAllMessagesInQueue(TestHelper.TestQueueName);
+
+            //assert
+            var finalMessageCount = TestHelper.getMessageCountInQueue();
+            Assert.AreEqual(finalMessageCount.ActiveMessageCount, 0);
+        }
+    }
+}
---
+++ VengabusAPI/VengabusAPI.Tests/TestHelper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace VengabusAPI.Tests
+{
+    public static class TestHelper
+    {
+        public const string sasString = "SharedAccessSignature sr=https%3A%2F%2Fvengabusdemo.servicebus.windows.net%2F&sig=3e1430xZG7nq41QsZNLdPIoF4L666UHb9GUlwZ9F8t8%3D&se=1533565396&skn=RootManageSharedAccessKey";
+
+        public const string ServiceBusURI = "sb://vengabusdemo.servicebus.windows.net/";
+
+        public const string TestQueueName = "backendtestqueue";
+        public const string TestTopicName = "backendtesttopic";
+        public const string TestSubscriptionName = "backendtestsubscription";
+
+        public static void SendDeadLetterMessagesToQueue(int messageCount, string queueName = TestQueueName)
+        {
+            var factory = CreateEndpointFactory();
+            var queueClient = factory.CreateQueueClient(queueName);
+
+            string msg = "This is a test message";
+            for (int i = 0; i < messageCount; i++)
+            {
+                var brokeredMessage = new BrokeredMessage(msg);
+                queueClient.Send(brokeredMessage);
+            }
+
+            for (int i = 0; i < messageCount; i++)
+            {
+                var message = queueClient.Receive();
+                message.DeadLetter();
+            }
+        }
+
+        public static void SendDeadLetterMessagesToSubscription(int messageCount, string topicName = TestTopicName, string subscriptionName = TestSubscriptionName)
+        {
+            var factory = CreateEndpointFactory();
+            var topicClient = factory.CreateTopicClient(topicName);
+            var subscriptionClient = factory.CreateSubscriptionClient(topicName, subscriptionName);
+
+            string msg = "This is a test message";
+            for (int i = 0; i < messageCount; i++)
+            {
+                topicClient.Send(new BrokeredMessage(msg));
+            }
+
+            for (int i = 0; i < messageCount; i++)
+            {
+                var message = subscriptionClient.Receive();
+                message.DeadLetter();
+            }
+        }
+
+        public static void SendMessagesToQueue(int messageCount, string queueName = TestQueueName)
+        {
+            var factory = CreateEndpointFactory();
+            var queueClient = factory.CreateQueueClient(queueName);
+
+            string msg = "This is a test message";
+            for (int i = 0; i < messageCount; i++)
+            {
+                queueClient.Send(new BrokeredMessage(msg));
+            }
+        }
+
+        public static void SendMessagesToTopic(int messageCount, string topicName = TestTopicName)
+        {
+            var factory = CreateEndpointFactory();
+            var topicClient = factory.CreateTopicClient(topicName);
+
+            string msg = "This is a test message";
+            for (int i = 0; i < messageCount; i++)
+            {
+                topicClient.Send(new BrokeredMessage(msg));
+            }
+        }
+
+        public static MessageCountDetails getMessageCountInQueue(string queueName = TestQueueName)
+        {
+            var namespaceManager = GetNamespaceManager();
+            var queueDescription = namespaceManager.GetQueue(queueName);
+            return queueDescription.MessageCountDetails;
+        }
+
+        public static MessageCountDetails getMessageCountInSubscription(string topicName = TestTopicName, string subscriptionName = TestSubscriptionName)
+        {
+            var namespaceManager = GetNamespaceManager();
+            var subscriptionDescription = namespaceManager.GetSubscription(topicName, subscriptionName);
+            return subscriptionDescription.MessageCountDetails;
+        }
+
+        private static NamespaceManager GetNamespaceManager()
+        {
+            var address = ServiceBusURI;
+            var token = TokenProvider.CreateSharedAccessSignatureTokenProvider(sasString);
+
+            var namespaceManager = new NamespaceManager(address, token);
+
+            return namespaceManager;
+        }
+
+        public static MessagingFactory CreateEndpointFactory()
+        {
+            var serviceBusUri = ServiceBusURI;
+            var sasToken = sasString;
+            var factory = MessagingFactory.Create(serviceBusUri, TokenProvider.CreateSharedAccessSignatureTokenProvider(sasToken));
+            return factory;
+        }
+    }
+}
