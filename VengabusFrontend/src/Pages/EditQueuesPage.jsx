e83cc0ee1be32db65f831c14111cf7f874bede6a
Mate Kovacs <Mate.Kovacs@softwire.com>
2018-08-07 11:23:05 +0100

    added example skeleton to the page

--- VengabusFrontend/src/Pages/EditQueuesPage.jsx
+++ VengabusFrontend/src/Pages/EditQueuesPage.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { serviceBusConnection } from '../AzureWrappers/ServiceBusConnection';
+import Select from 'react-select';
 
 export class EditQueuesPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedQueue: "demoqueue1"
+        };
+    }
+
     render() {
+        let queueData = serviceBusConnection.getServiceBusService().getQueueDetails(this.state.selectedQueue);
+        const permittedValues = [{ value: true, label: 'true' }, { value: false, label: 'false' }];
+        const supportOrdering = queueData.supportOrdering;
+
         return (
             <div>
-                
+                <p>SupportOrdering</p>
+                <Select
+                    title="Choose a value"
+                    options={permittedValues}
+                    value={supportOrdering ? { value: supportOrdering, label: supportOrdering.toString() } : undefined}
+                    onChange={(event) => console.log(event.value)}
+                />
             </div>
         );
     }
