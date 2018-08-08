a90b959ff132f8a025c70a7f83ab131404989d43
Mate Kovacs <Mate.Kovacs@softwire.com>
2018-08-08 16:23:21 +0100

    updated documentation and a small code improvement

--- VengabusFrontend/src/Components/DataTable.jsx
+++ VengabusFrontend/src/Components/DataTable.jsx
@@ -5,50 +5,49 @@ import { palerBlue, paleGreyBlue } from '../colourScheme';
 /*
 Returns a table created from an input object
 Props:
-    dataToDisplay: (REQUIRED) {array of objects} contains the data to be displayed in the table
-    colProps: (REQUIRED) {array of objects} contains properties regarding specific columns
-        dataField: (REQUIRED) {string} specifies the name of the property of DataToDisplay which is to be displayed in the column
-        text: (OPTIONAL) {string} Text which is to be displayed in the header of the column. If undefined then the default is going to use dataField => Data Field.
+    dataToDisplay: (REQUIRED) {array of objects} Contains the data to be displayed in the table.
+    colProps: (REQUIRED) {array of objects} Contains properties regarding specific columns.
+        dataField: (REQUIRED) {string} Specifies the name of the property of DataToDisplay which is to be displayed in the column.
+        text: (OPTIONAL) {string} Text which is to be displayed in the header of the column. If undefined then the default is going to use the value in dataField with the following transformation: myDataField => My Data Field.
         width: (OPIONAL) {number} Width of the column. If defined in one column then must be defined in all columns (excluding hidden ones) and overall width has to add up to 100%.
         headerStyle: (RECOMMENDED) {object} Contains CSS styling for the header. Width must be defined as above and not in here.
-        hidden: (OPTIONAL) {Boolean} If true then the column will be hidden. Default is false.
+        hidden: (OPTIONAL) {boolean} If true then the column will be hidden. Default is false.
         formatter: (OPTIONAL) {function} Can be used to render more complicated stuff within the cells, can change what to render based on current cell.
                     Takes arguments (cell, row, rowIndex) and should return a string or a JSX element to be rendered within the cell.
                     For example if you want to render a button within the column then you can pass the following to the formatter:
                     (cell, row, rowIndex) => {
                         return(
-                            <Button onClick={() => onClickFunction(row)}>
+                            <Button onClick={() => onClickFunction(cell, row, rowIndex)}>
                             Do something with the data in the row
                             </ Button>
                         )
                     }
-    tableRowStyle: (OPTIONAL) className for the rows
+                    Note that row is an object which contains all the information which is in that row.
     keyColumn: (REQUIRED) {string} dataField property of the column to be used as the key. If not unique then an error will be thrown.
-    rowEvents: (OPTIONAL) Object which contains functions that are called on certain events of the row. For example:
+    rowEvents: (OPTIONAL) {object} Contains functions that are called on certain events of the row. For example:
                 RowEvents = {
                     onClick: (row, rowIndex, e) => {
                         ....
                     }
                 }
-                where row is an object which contains all the information which is in that row
     rowClasses: (OPTIONAL) {string} CSS class that applies to the rows.
-    onRowClick: (OPTIONAL) Function to call when a row is clicked. Arguments are the same as above.
+    onRowClick: (OPTIONAL) {function} Called when a row is clicked. Arguments are the same as above.
                            Note that this is merely a more convenience wrapper for a common usages of rowEvents. It must not be defined in both.
-    bordered: (OPTIONAL) {Boolean} If false then no vertical borders. Default is true.
-    condensed: (OPTIONAL) {Boolean} If true then reduces padding in the table. Default is false.
-    defaultHover: (OPTIONAL) {Boolean} If true then default on hover styling will be applied.
-    selectRow: (OPTIONAL) defines settings relating to selecting a row by clicking on it.
-                selected: {int or String} This identifies a row to be rendered as currently Selected.
+    bordered: (OPTIONAL) {boolean} If false then no vertical borders. Default is true.
+    condensed: (OPTIONAL) {boolean} If true then reduces padding in the table. Default is false.
+    defaultHover: (OPTIONAL) {boolean} If true then default on hover styling will be applied.
+    selectRow: (OPTIONAL) {object} Defines settings relating to selecting a row by clicking on it.
+                selected: {int or string} This identifies a row to be rendered as currently Selected.
                           It is used by the table itself to persist selection status across re-renders, but can also be used by the developer to indicate an initial 'default' selection.
                           Note that setting an initial selection does NOT invoke any relevant onSelect functions - it ONLY does css etc.
                           Row to be selected is determined by:
                               If {int} then index (0-based, excluding header) of row.
                               If {string} then search for row matching to match text of the key Column.
                           The int option (index based initial selection) is implemented within this wrapper, by manually replacing the int with the appropriate string from the data.
-                style: {Object} Sets css style to be passed in to the selected row.
-                classes: {String} allows passing in css classes for styling (not an array)
-                bgColor: {String} sets background color of selected row, will throw an error if style or classes is defined
-                onSelect: {Function(row, isSelect, rowIndex, e)} called when row is selected (clicked)
+                style: {object} Sets css style to be passed in to the selected row.
+                classes: {string} Allows passing in css classes for styling (not an array).
+                bgColor: {string} Sets background color of selected row, will throw an error if style or classes is defined.
+                onSelect: {function(row, isSelect, rowIndex, e)} Called when row is selected (clicked).
     NOTES: 
             Every time a function will be called with one of the arguments being 'row' it will contain a 'key' property which is the index of the row.
 
@@ -149,15 +148,12 @@ export class DataTable extends Component {
                     const errorDataField = colProps[i].dataField;
                     throw new Error(`width of column should not be specified in the style (in ${name}:${errorDataField})`);
                 }
-            }
-
-            colProps = this.checkAndApplyWidth(colProps);
-
-            for (let i = 0; i < colProps.length; i++) {
                 if (!colProps[i].text) {
                     colProps[i].text = this.getTextFromDatafield(colProps[i].dataField);
                 }
             }
+
+            colProps = this.checkAndApplyWidth(colProps);
 
             if (rowEvents.onClick && onRowClick) {
                 throw new Error('the onClick event for rows is defined multiple times in ' + name);
