16c825dc0bb17458a77275fb8d26293c57d8ebfd
AliceWenban <wenbanalice@gmail.com>
2018-08-08 15:42:59 +0100

    made subscription test

---
+++ VengabusFrontend/src/__tests__/Components/SubscriptionList.test.js
@@ -0,0 +1,16 @@
+import { SubscriptionList } from '../../Components/SubscriptionList';
+import renderer from 'react-test-renderer';
+import React from 'react';
+
+describe('Subscription List', () => {
+
+    it('renders correctly with given props', () => {
+        const data = [{ name: 's1', activeMessageCount: 12, deadletterMessageCount: 14 }, { name: 's2', activeMessageCount: 11, deadletterMessageCount: 15 }, { name: 's3', activeMessageCount: 14, deadletterMessageCount: 16 }];
+        let subscriptionList = renderer.create(
+            <SubscriptionList
+                subscriptionData={data}
+            />);
+        expect(subscriptionList.toJSON()).toMatchSnapshot();
+    });
+
+});
