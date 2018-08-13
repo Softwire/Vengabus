2c40963cc834940a526c7c1cecd48b116bc281a5
AliceWenban <wenbanalice@gmail.com>
2018-08-13 09:38:05 +0100

    fixing test by changing stubbed data

--- VengabusFrontend/src/__tests__/Components/MessageList.test.js
+++ VengabusFrontend/src/__tests__/Components/MessageList.test.js
@@ -9,11 +9,13 @@ describe('MessageList', () => {
             {
                 predefinedProperties:
                     { messageId: 10 },
+                uniqueId: "2615ed49-94e0-4c27-9e44-39d1b8a28b26",
                 messageBody: "<shipto><name>Ola Nordmann</name><address>Langgt 23</address><city>4000 Stavanger</city><country>Norway</country></shipto><item><title>Empire Burlesque</title><note>Special Edition</note><quantity>1</quantity><price>10.90</price></item><item><title>Hide your heart</title><quantity>1</quantity><price>9.90</price></item></shiporder>"
             },
             {
                 predefinedProperties:
                     { messageId: 11 },
+                uniqueId: "fc6637ad-b19e-440a-b90e-ec7d277e6159",
                 messageBody: "banna"
             }];
         let messageList = renderer.create(
