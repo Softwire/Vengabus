15cc63fbcc9ab07e17ac2d06da2cda116f8f27c7
Ion Birsu <ion.birsu@softwire.com>
2018-08-13 18:02:41 +0100

    Add DeleteSingleMessage button

---
+++ VengabusFrontend/src/Components/DeleteSingleMessageButton.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Glyphicon } from 'react-bootstrap';
+import { serviceBusConnection } from '../AzureWrappers/ServiceBusConnection';
+import { EndpointTypes } from '../Helpers/EndpointTypes';
+import Lodash from 'lodash';
+import { ButtonWithConfirmationModal } from './ButtonWithConfirmationModal';
+
+class DeleteSingleMessageButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const vengaServiceBusService = serviceBusConnection.getServiceBusService();
+
+        this.initialState = {
+            onDeletionConfirmed: () => { },
+            modalBody: ""
+        };
+
+        switch (this.props.type) {
+            case EndpointTypes.TOPIC:
+                this.initialState.onDeletionConfirmed = () => vengaServiceBusService.deleteTopicSingleMessage(this.props.endpointName, this.props.messageId, this.props.uniqueId);
+                break;
+            case EndpointTypes.QUEUE:
+                this.initialState.onDeletionConfirmed = () => vengaServiceBusService.deleteQueueSingleMessage(this.props.endpointName, this.props.messageId, this.props.uniqueId);
+                break;
+            case EndpointTypes.SUBSCRIPTION:
+                this.initialState.onDeletionConfirmed = () => vengaServiceBusService.deleteSubscriptionSingleMessage(this.props.parentName, this.props.endpointName, this.props.messageId, this.props.uniqueId);
+                break;
+            default: break;
+        }
+
+        this.state = this.initialState;
+    }
+
+    showModalAction = () => {
+        this.generateModalWarningBody().then(bodyResult => this.setState({ modalBody: bodyResult }));
+    }
+
+    generateModalWarningBody = async () => {
+        return (
+            <React.Fragment>
+                <p>Are you sure you want to delete this message ?</p>
+
+                <p>"{this.messageId}" message will be deleted <b>irreversibly</b>!</p >
+            </React.Fragment>
+        );
+    }
+
+    resetState = () => {
+        this.setState({ modalBody: "" });
+    }
+
+    render() {
+        let buttonText = <span>Delete Message <Glyphicon glyph="trash" /></span>;
+        return (<ButtonWithConfirmationModal
+            id={"alertDelete"}
+            buttonText={buttonText}
+            modalTitle="Delete message"
+            modalBody={this.state.modalBody}
+            confirmButtonText={"Delete"}
+            afterShowModalAction={this.showModalAction}
+            confirmAction={this.state.onDeletionConfirmed}
+            afterCloseModalAction={this.resetState}
+        />);
+    }
+}
+
+export { DeleteSingleMessageButton };
