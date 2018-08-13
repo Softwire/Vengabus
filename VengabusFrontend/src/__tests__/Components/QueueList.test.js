84f42cda5409613d8ff7609a165598016d0cf250
AliceWenban <wenbanalice@gmail.com>
2018-08-13 09:04:02 +0100

    markups

--- VengabusFrontend/src/__tests__/Components/QueueList.test.js
+++ VengabusFrontend/src/__tests__/Components/QueueList.test.js
@@ -5,7 +5,10 @@ import React from 'react';
 describe('QueueList', () => {
 
     it('renders correctly with given props', () => {
-        const data = [{ name: 'q1', activeMessageCount: 12, deadletterMessageCount: 14 }, { name: 'q2', activeMessageCount: 11, deadletterMessageCount: 15 }, { name: 'q3', activeMessageCount: 14, deadletterMessageCount: 16}];
+        const data = [{ name: 'q1', activeMessageCount: 12, deadletterMessageCount: 14 },
+        { name: 'q2', activeMessageCount: 11, deadletterMessageCount: 15 },
+        { name: 'q3', activeMessageCount: 14, deadletterMessageCount: 16 }];
+
         let queueList = renderer.create(
             <QueueList
                 queueData={data}
--- VengabusFrontend/src/__tests__/Components/SubscriptionList.test.js
+++ VengabusFrontend/src/__tests__/Components/SubscriptionList.test.js
@@ -5,7 +5,10 @@ import React from 'react';
 describe('Subscription List', () => {
 
     it('renders correctly with given props', () => {
-        const data = [{ name: 's1', activeMessageCount: 12, deadletterMessageCount: 14 }, { name: 's2', activeMessageCount: 11, deadletterMessageCount: 15 }, { name: 's3', activeMessageCount: 14, deadletterMessageCount: 16 }];
+        const data = [{ name: 's1', activeMessageCount: 12, deadletterMessageCount: 14 },
+        { name: 's2', activeMessageCount: 11, deadletterMessageCount: 15 },
+        { name: 's3', activeMessageCount: 14, deadletterMessageCount: 16 }];
+
         let subscriptionList = renderer.create(
             <SubscriptionList
                 subscriptionData={data}
